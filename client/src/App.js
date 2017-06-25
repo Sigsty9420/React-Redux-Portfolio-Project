3707a704eeba7cdc4640e94c1102aae2fea1f63e
bortch89 <bortch@gmail.com>
2017-06-25 19:48:40 +0200

    Installed redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,16 @@ import Autosuggest from 'react-autosuggest';
 import {FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+  }
   render() {
     return (
       <div className="App">
@@ -14,8 +24,15 @@ class App extends Component {
               <FormControl
               type="text"
               placeholder="Search for a city"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                }
+              }}
               />
-              <InputGroup.Addon>
+              <InputGroup.Addon onClick={() => this.search()}>
                 <Glyphicon glyph="search"></Glyphicon>
               </InputGroup.Addon>
             </InputGroup>
