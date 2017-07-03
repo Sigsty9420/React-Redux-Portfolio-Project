47a64ee262600b1577a406754294816d3a9123fb
bortch89 <bortch@gmail.com>
2017-07-03 22:36:12 +0200

    finished get cities method for cities class to seed database with new cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,15 +3,18 @@ require 'rubygems'
 require 'pry'
 
 class City < ApplicationRecord
+  validates :name, uniqueness: true
+
   include HTTParty
   format :json
+
+
 
   def self.get_cities
     response = HTTParty.get("https://api.teleport.org/api/urban_areas/")
     if response.success?
       response['_links']['ua:item'].each do |city|
-        binding.pry
-        self.new()
+        self.create(name: city['name'], url: city['url'])
       end
     else
       raise response.response
