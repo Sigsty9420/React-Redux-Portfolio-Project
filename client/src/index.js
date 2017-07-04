4e824569948b8122b2ffde4e634ae013f5ddd4b3
bortch89 <bortch@gmail.com>
2017-07-04 14:54:04 +0200

    installed thunk module and imported it and some redux methods into the main index.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
