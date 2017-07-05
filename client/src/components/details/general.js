bcb40e25c082d2d46ca0f000d1db8683a34f6894
bortch89 <bortch@gmail.com>
2017-07-05 11:11:15 +0200

    Added more components to create eventual main city view

--- client/src/components/details/general.js
+++ client/src/components/details/general.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default (props) => {
   return (
-    <div className="container col-md-4 col-md-offset-4">
-      <h2 className="text-center">General information:</h2>
+    <div className="container col-md-4">
+      <h2 className="text-center">General information</h2>
       <p>Full name - {props.name}</p>
       <p>Population - {props.population} million</p>
     </div>
--- client/src/containers/MainCity.js
+++ client/src/containers/MainCity.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import General from '../components/general'
+import General from '../components/details/general'
 
 class MainCity extends Component {
   render(){
     return (
       <div className="row">
         <h2 className="text-center">{this.props.city.name}</h2>
+        <div className="col-md-offset-4">
         <General name={this.props.city.name} population={this.props.city.urban_area_population_millions} />
+        </div>
       </div>
     );
   }
