ab69141be9f78b45aff7e9df9deba79ea0017069
bortch89 <bortch@gmail.com>
2017-07-08 09:52:24 +0200

    Added city voting feature

--- client/src/components/FavouriteCity.js
+++ client/src/components/FavouriteCity.js
@@ -6,16 +6,25 @@ import CityDropDown from '../containers/CityDropDown';
 
 
 class FavouriteCity extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {username: ''};
 
-    this.onInputChange = this.onInputChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      votedCity: {},
+    };
+
+
     this.onFormSubmit = this.onFormSubmit.bind(this);
+
   }
 
-  onInputChange(event){
-    this.setState({username: event.target.value})
+  onInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    })
   }
 
   onFormSubmit(event) {
@@ -23,42 +32,61 @@ class FavouriteCity extends Component {
     this.props.getUserCity(this.state.username);
   }
 
+  upVote = city => {
+    if (city.name in this.state.votedCity ) {
+      let tempCity = this.state.votedCity
+      tempCity[city.name] = tempCity[city.name] + 1
+      this.setState({
+        votedCity: tempCity
+      })
+    } else {
+      let tempCity = this.state.votedCity
+      tempCity[city.name] = 1
+      this.setState({votedCity: tempCity})
+    }
+  }
+
   render() {
     const { userCities } = this.props;
-      return (
-        <div className="col-md-offset-4 col-md-4">
+
+    console.log(this.state)
+
+    return (
+      <div className="col-md-offset-4 col-md-4">
+        <br />
+        <form className="input-group" onSubmit={this.onFormSubmit}>
+          <input
+            name="username"
+            placeholder="Please enter your username"
+            className="form-control"
+            onChange={this.onInputChange}
+          />
           <br />
-          <form className="input-group" onSubmit={this.onFormSubmit}>
-            <input
-              placeholder="Please enter your username"
-              className="form-control"
-              onChange={this.onInputChange}
-              />
-            <br />
-            <br />
-              <button type="submit" className="btn btn-secondary">Submit</button>
-          </form>
-          <CityDropDown username={this.state.username} />
+          <br />
+            <button type="submit" className="btn btn-secondary" >Submit</button>
+        </form>
 
-          { userCities && userCities.map((value, key) => {
-            return (
-              <div key={key}>
-                <p>{value.name}</p>
-              </div>
-            )
-          })}
-        </div>
-      )
+        <CityDropDown username={this.state.username} />
+
+        { userCities &&
+          userCities.map((city, key) =>
+            <div key={key}>
+              <p>{city.name} - {this.state.votedCity[city.name]}</p>
+              <button onClick={() => this.upVote(city)}
+                className="btn btn-secondary"
+              >
+                Submit
+              </button>
+            </div>
+          )
+        }
+      </div>
+    )
   }
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ getUserCity }, dispatch)
 }
 
 function mapStateToProps(state){
   return {userCities: state.cities.userCities }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(FavouriteCity);
+export default connect(mapStateToProps, { getUserCity })(FavouriteCity);
